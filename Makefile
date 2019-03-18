5c07f68befcdec702849c664a4ac1d531a6e99a1
Noura Djaffri <nouradjaffri@gmail.com>
2019-03-18 18:49:25 -0400

    Ajout de Type: Bien insérer dans l'arbre + affiche le contenu du type

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CARGS= -lm -std=c++11
 
 all : testavl tp2 
 
-default: tp2
+default: tp2 arbreavl.h fonctor.h type.h
 tp2:
 	@g++ tp2.cpp $(CARGS)
 
--- tp2.cpp
+++ tp2.cpp
@@ -2,19 +2,47 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
 #include "arbreavl.h"
+#include "type.h"
 
 using namespace std;
 
-int main(int argc, const char** argv){    
-    ifstream fichier(argv[1], ios::in); 
 
-    if(fichier){
-        fichier.close(); 
+
+int main(int argc, const char** argv){    
+    vector<string> toto;
+    toto.push_back("Hello"); 
+    toto.push_back("Chat"); 
+    toto.push_back("Chien"); 
+    toto.push_back("poto");  
+    Type *type= new Type("Toto", toto); 
+
+    ArbreAVL<Type> *tree = new ArbreAVL<Type>(); 
+
+    tree->inserer(*type); 
+    Type *type2 = new Type("coco", toto);
+    tree->inserer(*type2); 
+
+    if(tree->contient (*type)){
+        cout << *type2 << endl; 
     }
-    else{
-        cerr << "Impossible d'ouvrir le fichier." << endl; 
-    }
+     
+
+    
+
+
+    // ifstream fichier(argv[1], ios::in); 
+
+    // if(fichier)
+    // {
+    //     //process l'entrée
+    //     fichier.close();
+    // }
+    // else{
+    //     cerr << "Impossible d'ouvrir le fichier." << endl; 
+    // }
 
 }
    
