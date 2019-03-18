a54ff40e63db3a87ea39703370827049580c40f9
Noura Djaffri <nouradjaffri@gmail.com>
2019-03-18 09:52:52 -0400

    read file and close it

--- tp2.cpp
+++ tp2.cpp
@@ -1,11 +1,20 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 #include "arbreavl.h"
 
 using namespace std;
 
-int main(int argc, const char** argv){
-     cout << "Hello World" << endl; 
+int main(int argc, const char** argv){    
+    ifstream fichier(argv[1], ios::in); 
+
+    if(fichier){
+        fichier.close(); 
+    }
+    else{
+        cerr << "Impossible d'ouvrir le fichier." << endl; 
+    }
+
 }
    
