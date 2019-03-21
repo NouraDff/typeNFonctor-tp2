f8da3b2ba9196d676b414de69126f4668b0e3e3d
Laurianne Guindon <hh591051@ens.uqam.ca>
2019-03-21 12:22:07 -0400

    test

--- tp2.cpp
+++ tp2.cpp
@@ -40,6 +40,7 @@ int main(int argc, const char **argv)
 					cerr << "Les arguments ne sont pas tous uniques." << endl; // arrêt??
 				else
 					t.pop_back();
+:wq
 
 				for (unsigned it = 0; it < t.size(); ++it)
 					cout << t.at(it) << endl;
@@ -73,31 +74,81 @@ int main(int argc, const char **argv)
 
 					clause.push_back(strtok(ligne, "( ,"));
 					int i = 0;
-					while (c.back() != NULL && find(c.begin(), c.end(), c.back()) == c.end() - 1 /*&& c.back() est un arg de t.at(i++)*/) // lettres min
-						c.push_back(strtok(NULL, " ,)"));
-					if (c.back() != NULL && find(c.begin(), c.end(), c.back()) != c.end() - 1)
-						cerr << "Les arguments ne sont pas tous uniques." << endl; // arrêt??
-					else
-						c.pop_back();
+					while (c.back() != NULL && find(c.begin(), c.end(), c.back()) == c.end() - 1 /*&& c.bac
+int main(int argc, const char** argv){    
+    ifstream fichier(argv[1], ios::in); 
 
-					for (unsigned it = 0; it < c.size(); ++it)
-						cout << c.at(it) << endl;
+    if(fichier){
 
-					f.push_back(c);
-					c.clear();
-					c.shrink_to_fit();
-				}
-				// créer obj
-				f.clear();
-				f.shrink_to_fit();
+	vector<char*> t, c;
+	string entree, nom, str;
+
+	while(fichier >> entree >> nom){	// être sûr que tt est en lettres min!! (caract par caract: isalpha(car) && islower(car)
+cout << entree << "->" << nom << endl;
+		if(!entree.compare("type")){
+		//treeType.rechercher(<"nom">); veut voir s'il existe avant de l'ajouter...
+                	getline(fichier, str);
+			char* typ = new char[str.length()+1];
+                        strcpy(typ, str.c_str());
+
+                        t.push_back(strtok(typ, "= {,"));
+                        while(t.back() != NULL && find(t.begin(), t.end(), t.back())==t.end()-1) // lettres min
+                                t.push_back(strtok(NULL, " ,}"));
+			if(t.back()!=NULL && find(t.begin(), t.end(), t.back())!=t.end()-1)
+				cerr << "Les arguments ne sont pas tous uniques." << endl; // arrêt??
+			else t.pop_back();
+
+			for(unsigned it = 0 ; it < t.size(); ++it)
+                                cout << t.at(it) << endl;
+			// créer objet
+		}else if(!entree.compare("foncteur")){
+		//treeFonctor.rechercher(<"nom">); veut voir s'il existe avant de l'ajouter...
+                        getline(fichier, str);	
+			char* typ = new char[str.length()+1];
+			strcpy(typ, str.c_str()); 
+
+			t.push_back(strtok(typ, ": ,"));
+			while(t.back() != NULL && find(t.begin(), t.end(), t.back())==t.end()-1 /*&& t.back() est dans l'arbre de type*/) // lettres min
+				t.push_back(strtok(NULL, " ,"));
+			if(t.back()!=NULL && find(t.begin(), t.end(), t.back())!=t.end()-1)
+                                cerr << "Les arguments ne sont pas tous uniques." << endl; // arrêt??
+			else t.pop_back();
+
+			for(unsigned it = 0 ; it < t.size(); ++it)
+                                cout << t.at(it) << endl;
+
+			vector<vector<char*>> f;
+			char* fonc;
+			while(fichier.peek() == '('){
+				getline(fichier, str);
+				fonc = new char[str.length()+1];
+				strcpy(fonc, str.c_str());
+
+				c.push_back(strtok(fonc, "( ,"));
+				int i = 0;
+				while(c.back() != NULL && find(c.begin(), c.end(), c.back())==c.end()-1 /*&& c.back() est un arg de t.at(i++)*/) // lettres min
+                                	c.push_back(strtok(NULL, " ,)"));
+				if(c.back()!=NULL && find(c.begin(), c.end(), c.back())!=c.end()-1)
+                                	cerr << "Les arguments ne sont pas tous uniques." << endl; // arrêt??
+				else c.pop_back();
+
+				for(unsigned it = 0 ; it < c.size(); ++it)
+                                	cout << c.at(it) << endl;
+
+				f.push_back(c);
+				c.clear();
+				c.shrink_to_fit();
 			}
-			else
-			{
-				cerr << "Entrée invalide." << endl; // arrêt??
-			}
-			t.clear();
-			t.shrink_to_fit();
+			// créer obj
+			f.clear();
+			f.shrink_to_fit();
+		}else{
+			cerr << "Entrée invalide." << endl; // arrêt??
 		}
+		t.clear();
+		t.shrink_to_fit();
+	}
+>>>>>>> parent of f1c3f91... Modif: jeu 21 mar 2019 à 11:38:21
 
 		fichier.close();
 	}
